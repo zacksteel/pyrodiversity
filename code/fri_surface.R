dd32abb58fcd7d246660a647ec0e393eda7f91d3
zacksteel <zlsteel@berkeley.edu>
2022-03-03 15:58:21 -0800

    updating fri_surface.R with terra

--- code/fri_surface.R
+++ code/fri_surface.R
@@ -13,15 +13,16 @@ fri_surface <- function(landscape, # feature(s) that represent the landscape of 
   ) {
   library(tidyverse)
   library(sf)
-  library(raster)
+  library(terra)
   library(stars)
-  library(fasterize)
+  # library(fasterize)
+  library(here)
   
   ## filename of output
   fn_r <- paste0(out_dir, "/fri_", as.data.frame(landscape)[1,ID], ".tif")
   
   ## Pull in severity raster to use as template
-  r_template <- raster(raster_template)
+  r_template <- rast(here(raster_template))
   
   ## Conform CRS of features to the template
   landscape <- st_transform(landscape, crs = st_crs(r_template))
@@ -34,20 +35,24 @@ fri_surface <- function(landscape, # feature(s) that represent the landscape of 
   
   ## assign fire year column name
   fires <- rename(fires, Fire_Year = !! (sym(fire_years)))
+  # fires <- rename(fires, Fire_Year = Year)
 
   ## landscape raster of start year
-  land_r <- as_Spatial(landscape) %>% 
-    raster(resolution = res(r_template), vals = start_year) %>% 
-    mask(landscape)
+  # land_r <- as_Spatial(landscape) %>% 
+  #   raster(resolution = res(r_template), vals = start_year) %>% 
+  #   mask(landscape)
+  land_r <- vect(landscape) %>% 
+    rast(resolution = res(r_template), vals = start_year) %>% 
+    mask(vect(landscape))
   ## and end year
-  end_r <- as_Spatial(landscape) %>% 
-    raster(resolution = res(r_template), vals = end_year) %>% 
-    mask(landscape)
+  end_r <- vect(landscape) %>% 
+    rast(resolution = res(r_template), vals = end_year) %>% 
+    mask(vect(landscape))
 
   ## FRI raster without fires
-  noburn_r <- as_Spatial(landscape) %>%
-    raster(resolution = res(r_template), vals = end_year - start_year) %>% 
-    mask(landscape)
+  noburn_r <- vect(landscape) %>%
+    rast(resolution = res(r_template), vals = end_year - start_year) %>% 
+    mask(vect(landscape))
   
   ## If no fires within the landscape skip a lot and treat the landscape as a single patch
   if(nrow(fires) == 0) {
@@ -66,32 +71,39 @@ fri_surface <- function(landscape, # feature(s) that represent the landscape of 
       sort()
     
     ## Created a raster for each year there was at least one fire
-    #### Slow step here, stars or terra versions of rasterize and resample might help
     fri_yrs <- lapply(years, function(x) {
       r <- filter(fires_land, Fire_Year == x) %>% 
         st_collection_extract("POLYGON") %>% 
-        fasterize(raster = land_r, field = "Fire_Year") %>% 
+        vect() %>% 
+        terra::rasterize(y = land_r, field = "Fire_Year") %>% 
         suppressWarnings()
       
       ## Align with the landscape raster
-      return(raster::resample(r, land_r))
+      return(terra::resample(r, land_r))
       
     })
     
     ## Add start & end year raster
-    fri_yrs <- c(land_r, fri_yrs, end_r)
+    fri_yrs <- append(list(land_r), fri_yrs) %>% 
+      append(list(end_r))
     
     ## Get years since last fire for each year
     ## set up empty raster
-    ysf_yrs <- lapply(fri_yrs, function(x) x * NA)
+    ysf_yrs <- lapply(fri_yrs, function(x) setValues(x, NA) )
     for(i in 2:length(fri_yrs)) {
       ## make raster of most recent event
       #### stars or terra version of calc may improve speed here as well
-      rc <- calc(stack(fri_yrs[i-1:i]), fun = max)
+      # rc <- calc(stack(fri_yrs[i-1:i]), fun = max)
+      ## terra version
+      rc <- rast(fri_yrs[i-1:i]) %>% 
+        app(fun = max, na.rm = T)
+
       ## years since most resent fire
       r <- fri_yrs[[i]] - rc
       ysf_yrs[[i]] <- r
     }
+    ## add first year back in as our backstop
+    # ysf_yrs[[1]] <- setValues(ysf_yrs[[1]], 0)
     
     ## Assign burned areas as 1
     burned_l <- lapply(ysf_yrs, function(x) {
@@ -99,8 +111,13 @@ fri_surface <- function(landscape, # feature(s) that represent the landscape of 
       x[is.na(x)] <- 0
       return(x)
     })
-    ## Add them up to get the cummulative number of fires
-    fire_cnt <- Reduce("+", burned_l, accumulate = T)
+    burned_l[[1]] <- setValues(ysf_yrs[[1]], NaN)
+    
+    ## Add them up to get the cumulative number of fires
+    ## Skip first empty year and add back in
+    # fire_cnt <- Reduce("+", lapply(burned_l, raster), accumulate = T)
+    fire_cnt <- Reduce("+", burned_l[2:length(burned_l)], accumulate = T)
+    fire_cnt <- append(burned_l[1], fire_cnt)
     
     ## Convert back to NA if a pixel didn't burn in a given year
     fire_order <- lapply(1:length(burned_l), function(x) {
@@ -108,26 +125,29 @@ fri_surface <- function(landscape, # feature(s) that represent the landscape of 
       x[x == 0] <- NA
       return(x)
     })
+    # fire_order <- append(burned_l[1], fire_order)
     
     ## Created a stack of weight rasters 
     ## create raster of max fires so we are flipping the weight and 
     ## making recent burns more important
-    stck <- stack(fire_order)
-    maxorder <- if(nlayers(stck) == 1) {1} else {
+    # stck <- stack(fire_order)
+    stck <- rast(fire_order)
+    maxorder <- if(nlyr(stck) == 1) {1} else {
       max(stck, na.rm = T)
     }
     
     w <- lapply(fire_order, function(x) {
-      ## exponentiating to zero gives a weight of 1, higher numbers get lower weights
+      ## exponentiating to zero gives a weight of 1 (in the case of the most recent interval; btwn last year and last fire), higher numbers get lower weights
+      ## exponent term makes the fire order relative the max (e.g., if there have been 3 fires and we are on the third it gets full weight)
       (1 - decay_rate) ^ (maxorder - x)
     }) %>%
-      stack()
+      rast()
     
     ## stack ysf layers
-    ysf_stack <- stack(ysf_yrs)
+    ysf_stack <- rast(ysf_yrs)
     
     ## Get weighted average
-    weighted_fri <- if(nlayers(ysf_stack) == 1) {ysf_stack[[1]]} else {
+    weighted_fri <- if(nlyr(ysf_stack) == 1) {ysf_stack[[1]]} else {
       weighted.mean(ysf_stack, w, na.rm = T)}
     
 
