36234f526a8af39b01a7ec4ec9223d1fb472c316
zacksteel <zacksteel@gmail.com>
2023-12-05 14:32:23 -0700

    fixing bugs in point_fd

--- code/point_fd.R
+++ code/point_fd.R
@@ -5,19 +5,21 @@ point_fd <- function(
   points, # sf or vect object of points
   traits, #vector of paths to trait rasters or rast objects
   tr_wt = NULL, # relative weights for traits
-  land_radius, # radius of the local landscape to consider for functional diversity, units are those of the length unit of the trait crs
+  land_radius, # radius of the local landscape to consider for functional diversity, units are those of the length unit of the trait crs (usually meters)
   frich = F, #logical, whether to also calculate function richness
   pca_axes = "max", #number of PC dimensions to use when calculating FRic
-  mask = NULL #optional mask layer path (e.g. remove non-flammable areas)
+  mask = NULL, #optional mask layer path (e.g. remove non-flammable areas)
+  proot = getwd() #optional project root needed to load global_fd.R; temporary work around until this is a formal package
   )
 {
   ## read in necessary libraries and global_fd function
   library(tidyverse, quietly = T, warn.conflicts = F, verbose = F)
   library(sf, quietly = T, warn.conflicts = F, verbose = F)
   library(terra, quietly = T, warn.conflicts = F, verbose = F)
+  library(here, quietly = T, warn.conflicts = F, verbose = F)
   
   #### sensitive to location of this function; need to fix if making into a package
-  source(here("code/global_fd.R"))
+  source(paste0(proot, "/code/global_fd.R"))
   
   ## Read in rasters
   if(class(traits) != 'SpatRaster') {
@@ -45,26 +47,28 @@ point_fd <- function(
   for(i in 1:length(pts.buf)) {
     pt <- pts.buf[i,]
     
-    ts.sm <- crop(ts, pt) %>% 
-      # touches = F means that a cell whose center is not within the circle is excluded
-      mask(pt, touches = F)
+    ## limit to local landscape around the point
+    ts.sm <- crop(ts, pt) 
+    # touches = F means that a cell whose center is not within the circle is excluded
+    ts.sm <- mask(ts.sm, pt, touches = F) 
     
     ## if no variation left in a trait drop it
     mm <- minmax(ts.sm)
     mm <- mm[2,] - mm[1,]
-    keep <- ifelse(mm == 0, FALSE, TRUE)
+    keep <- ifelse(mm == 0 | is.na(mm), FALSE, TRUE)
     
     ## if none left move on
     if(TRUE %in% keep) {
       ts.sm <- ts.sm[[keep]]
       ## same for trait weights if not null
-      if(!is.null(tr_wt)) {tr_wt <- tr_wt[keep]}
+      if(!is.null(tr_wt)) {tr_wt2 <- tr_wt[keep]} else
+        if(is.null(tr_wt)) {tr_wt2 <- NULL}
       
       nts <- nlyr(ts.sm)
       
       ## Run through global_fd
       d.sub <- global_fd(traits = ts.sm,
-                         tr_wt = tr_wt,
+                         tr_wt = tr_wt2,
                          frich = frich,
                          pca_axes = pca_axes,
                          mask = mask)
